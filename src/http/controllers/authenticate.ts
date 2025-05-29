f06fdeb5329932f4ca1c11afba083ae83cc245f2
wagngomes <wagner.gomes.cx2@gmail.com>
2025-05-29 17:15:43 -0300

    feat: tests e2e

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -15,7 +15,18 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     try{
         const authenticateUseCase = makeAuthenticateUseCase()
 
-        await authenticateUseCase.execute({ email, password})
+        const { user } = await authenticateUseCase.execute({ email, password})
+
+        const token = await reply.jwtSign(
+            {},
+            {
+                sign: {
+                    sub: user.id
+                },
+            },
+        )
+
+        return reply.status(200).send({token,})
 
     }catch(err){
 
@@ -25,5 +36,5 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
         
         throw err
     }
-    return reply.status(200).send()
+   
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,16 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
+import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance){
 
     app.post('/users', register)
     app.post('/sessions', authenticate)
+
+    //Rotas privadas
+
+    app.get('/me',{ onRequest: [verifyJWT]},profile)
 
 }
